c6fc00d637db6002252c6a34f1503aa8b15c91d7
Parsa <34625096+parsakhavarinejad@users.noreply.github.com>
2024-05-05 15:18:07 +0330

    Update mydataset.py

--- ACDC/mydataset.py
+++ ACDC/mydataset.py
@@ -5,37 +5,32 @@ from torchvision import datasets, transforms
 import os
 from PIL import Image, ImageOps
 
-class CVCClinicDB_Dataset(data.Dataset):
-    def __init__(self, dataset_type='train', transform=None):
-        self.item_image = sorted(os.listdir("Dataset/archive/PNG/Original"))
-        self.item_gt = sorted(os.listdir("Dataset/archive/PNG/GroundTruth"))
-
-        self.transform = transform
-
-        if dataset_type=='train':
-            self.images = self.item_image[:368]
-            self.labels = self.item_gt[:368]
-        elif dataset_type=='val':
-            self.images = self.item_image[368:490]
-            self.labels = self.item_gt[368:490]
-        elif dataset_type=='test':
-            self.images = self.item_image[490:]
-            self.labels = self.item_gt[490:]
-
-    def __getitem__(self, index):
-        img_name = self.images[index]
-        label_name = self.labels[index]
-        image = Image.open("Dataset/archive/PNG/Original/" + img_name).convert("RGB")
-        label = Image.open("Dataset/archive/PNG/GroundTruth/" + label_name).convert("L")
-        label = np.array(label)
-        mask = np.where(label>200, 1, 0)
-        mask = Image.fromarray(np.uint8(mask))
-
-        if self.transform:
-            image, mask = self.transform(image, mask)
-        return image, mask
+class CustomImageMaskDataset(data.Dataset):
+    def __init__(self, dataframe, image_transform=None):
+        self.data = dataframe
+        self.image_transform = image_transform
 
     def __len__(self):
-        return len(self.images)
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        image_path = self.data.iloc[idx]['image_path']
+        mask_path = self.data.iloc[idx]['mask_path']
+
+        image = Image.open(image_path).convert('L') 
+        mask = Image.open(mask_path).convert('L')
+        
+        seed = np.random.randint(2147483647)
+        random.seed(seed) 
+        torch.manual_seed(seed) 
+        if self.image_transform:
+            image = self.image_transform(image)
+        
+        random.seed(seed) 
+        torch.manual_seed(seed)
+        if self.image_transform:
+            mask = self.image_transform(mask)
+
+        return image, mask
 
 
